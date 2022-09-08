826399e67632e6d51994f7ea8924d833ae54ef35
AntonSvirid <adsvirid@tut.by>
2022-09-08 09:32:31 +0300

    updated Task01

--- src/Task01.java
+++ src/Task01.java
@@ -2,5 +2,10 @@ public class Task01 {
     public static void main(String[] args) {
 
         System.out.println("Testing GitHub...");
+        System.out.println("Testing GitHub...");
+        System.out.println("Testing GitHub...");
+        System.out.println("Testing GitHub...");
+        System.out.println("Testing GitHub...");
+
     }
 }
