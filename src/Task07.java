e49b355f91cf8f90dbe40229bf8251a29d3b6590
AntonSvirid <adsvirid@tut.by>
2022-09-08 11:59:18 +0300

    Update mood detector with const fields

--- src/Task07.java
+++ src/Task07.java
@@ -4,30 +4,35 @@
 // :() - shouted
 // :\ - nervous
 
-
-import javax.sound.midi.Soundbank;
-import java.sql.SQLOutput;
 import java.util.Random;
 
 public class Task07 {
+    public static final int MOOD_NUMBER = 5;
+
+    public static final String HAPPY_MOOD = ":)";
+    public static final String BAD_MOOD = ":(";
+    public static final String NEURAL_MOOD = ":|";
+    public static final String NERVOUS_MOOD = ":/";
+    public static final String SHOUTED_MOOD = ":()";
+
     public static void main(String[] args) {
         Random random = new Random();
 
-        int mood = random.nextInt(5);
-        String msg = ":)";
+        int mood = random.nextInt(MOOD_NUMBER);
+        String msg = HAPPY_MOOD;
 
         switch (mood) {
             case 1:
-                msg = ":(";
+                msg = BAD_MOOD;
                 break;
             case 2:
-                msg = ":|";
+                msg = NEURAL_MOOD;
                 break;
             case 3:
-                msg = ":()";
+                msg = SHOUTED_MOOD;
                 break;
             case 4:
-                msg = ":/";
+                msg = NERVOUS_MOOD;
                 break;
         }
         System.out.print(msg);
