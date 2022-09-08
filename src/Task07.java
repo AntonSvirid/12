8b200163caddb3bc49e2d1d40ef4473b0f4d5520
AntonSvirid <adsvirid@tut.by>
2022-09-08 11:43:22 +0300

    Add mood detector

--- src/Task07.java
+++ src/Task07.java
@@ -1,5 +1,35 @@
+// :) - happy
+// :( - bad
+// :| - neutral
+// :() - shouted
+// :\ - nervous
+
+
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
+import java.util.Random;
+
 public class Task07 {
     public static void main(String[] args) {
+        Random random = new Random();
 
+        int mood = random.nextInt(5);
+        String msg = ":)";
+
+        switch (mood) {
+            case 1:
+                msg = ":(";
+                break;
+            case 2:
+                msg = ":|";
+                break;
+            case 3:
+                msg = ":()";
+                break;
+            case 4:
+                msg = ":/";
+                break;
+        }
+        System.out.print(msg);
     }
 }
